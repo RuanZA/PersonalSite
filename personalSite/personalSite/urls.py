9f2bc313738a63cb5f9a05f67c7444299f92a44c
RuanZA <ruanerasmus29@gmail.com>
2019-12-10 16:35:25 +0200

    added users apps

--- personalSite/personalSite/urls.py
+++ personalSite/personalSite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from personalSite.users import views as user_views
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
